5ebc2c99b34e38a4f4027e6141ddbcc3f117e3e8
EduardoFernandezEvangelista <eduardofernandezevangelista100@hotmail.com>
2026-09-21 10:14:29 -0300

    first commit to GreenDay

---
+++ GreenDay/src/main/java/br/com/senai/greenday/GreenDayApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.greenday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenDayApplication.class, args);
+    }
+
+}
